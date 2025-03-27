53266988fa0bf9f686c2bc2b86c38d72bc5c9c48
a26363857 <a26363857@gmail.com>
2025-03-27 11:38:19 +0900

    新增一個測試程式碼 驗證 A+B

---
+++ Assets/Script.Tests/CSharpPracticeTests.cs
@@ -0,0 +1,20 @@
+using System.Security.Cryptography;
+using Microsoft.CSharp;
+using NUnit.Framework;
+
+public class CSharpPracticeTests
+{
+    [Test(Description = "兩數字的相加")]
+    [TestCase(2, 2, 4)]
+    [TestCase(3, 3, 6)]
+    [TestCase(10, 10, 20)]
+    [TestCase(-10, 20, 10, Description = "負數與正數的相加")]
+    public void Add_Two_number(int a, int b, int expected)
+    {
+        CsharpPractice csharpPractice = new CsharpPractice();
+        int result = csharpPractice.Add(a, b);
+        Assert.AreEqual(expected, actual:result );
+    }
+    
+    
+}
